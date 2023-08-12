44e5984db7513c11d35a0cd4dd51ab0c7df42d97
Anothermeer <86600894+anothermeer@users.noreply.github.com>
2023-08-12 15:56:25 +0800

    Update MinecraftServerInstaller_1.0.0.bat

--- MinecraftServerInstaller_1.0.0.bat
+++ MinecraftServerInstaller_1.0.0.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZksaHGQ=
-::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbAs=
-::ZQ05rAF9IAHYFVzEqQIdIRVRTxaDOn+JA6cK7frS4fmTo0ERUfBf
-::eg0/rx1wNQPfEVWB+kM9LVsJDCmLMm65FLQa/Nv+/fyCsGQTR/Y+dIPeyNQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDCmhD0L0A60ZiA==
-::cRolqwZ3JBvQF1fEqQIRaB5VXx3CMWK0A7YO6e7vr/mCsFsYRqI2dpzP27iCJfJz
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFBpQQQ2MAE+/Fb4I5/jHwuOJp04PVeQrS4rJzLGcCe4A/kToeZM/6SsUyIVcQh5Ae3I=
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 
 rem The Creator is a furry :)
